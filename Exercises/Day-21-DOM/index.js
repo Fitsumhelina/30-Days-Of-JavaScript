1c2314de38b22162c68237ec285d03386cc3b9f7
Fitsumhelina <permin1501424@gmail.com>
2024-12-21 23:57:38 +0300

    second excersise complated

--- Exercises/Day-21-DOM/index.js
+++ Exercises/Day-21-DOM/index.js
@@ -9,6 +9,7 @@
 
 document.addEventListener('DOMContentLoaded', () => {
     function1()
+    function2()
 })
 
 const function1 = () => {
@@ -34,12 +35,25 @@ const function1 = () => {
 // Change stye of each paragraph using JavaScript(eg. color, background, border, font-size, font-family)
 // Select all paragraphs and loop through each elements and give the first and third paragraph a color of green, and the second and the fourth paragraph a red color
 // Set text content, id and class to each paragraph
+
+const function2 = () => {
+    const allparagraphs  = document.querySelectorAll('p')
+    for ( i =0 ; i<allparagraphs.length ; i++ ) {
+        if (i%2 === 0) {
+            allparagraphs[i].style.backgroundColor = 'green'
+        } else {
+            allparagraphs[i].style.backgroundColor = 'red'
+        }
+    }
+}
+
 // Exercise: Level 3
 // DOM: Mini project 1
 // Develop the following application, use the following HTML elements to get started with. You will get the same code on starter folder. Apply all the styles and functionality using JavaScript only.
-
 // The year color is changing every 1 second
 // The date and time background color is changing every on seconds
 // Completed challenge has background green
 // Ongoing challenge has background yellow
 // Coming challenges have background red
+
+
